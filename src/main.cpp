d71c1d7b48942f5524ab92b955d175948af97a36
MrJujek <julian.dworzycki@gmail.com>
2023-07-14 12:45:41 +0200

    :sparkles: Make window with generated terrain

--- src/main.cpp
+++ src/main.cpp
@@ -10,5 +10,62 @@ int main(int, char **)
 {
     GenerateTerrain generatedTerrain;
 
+    const int rows = size;
+    const int columns = size;
+    const int squareSize = 3;
+
+    const int screenWidth = columns * squareSize;
+    const int screenHeight = rows * squareSize;
+
+    InitWindow(screenWidth, screenHeight, "Kwadraty zależne od wartości");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                int value = generatedTerrain.terrain[i][j];
+                Color color;
+
+                // Przypisanie koloru w zależności od wartości
+                switch (value)
+                {
+                case 0:
+                    color = BLUE;
+                    break;
+                case 1:
+                    color = YELLOW;
+                    break;
+                case 2:
+                    color = GREEN;
+                    break;
+                case 3:
+                    color = GRAY;
+                    break;
+                case 4:
+                    color = BLACK;
+                    break;
+                default:
+                    color = WHITE;
+                    break;
+                }
+
+                // Rysowanie kwadratu o odpowiednim kolorze
+                DrawRectangle(j * squareSize, i * squareSize, squareSize, squareSize, color);
+            }
+        }
+
+        EndDrawing();
+    }
+
+    // CloseWindow();
+
     makeTerrainImage(generatedTerrain.terrain);
 }
